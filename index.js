7c8a43338c11ec7b389dd87a475dca695a208e52
WorkiLogic <157142769+WorkiLogic@users.noreply.github.com>
2024-08-27 23:50:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    store: new FileStore(),
+    store: new FileStore({ path: path.join(__dirname, 'sessions') }),
     secret: secretKey,
     resave: false,
     saveUninitialized: false,
